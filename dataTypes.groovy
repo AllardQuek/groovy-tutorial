aa517e88073b0a1ec9e4881ad137d824cb9f2800
Allard <AllardQuek@users.noreply.github.com>
2022-05-10 15:27:35 +0800

    Finish data types

--- dataTypes.groovy
+++ dataTypes.groovy
@@ -2,6 +2,18 @@ byte b = 10
 println b
 println "range of byte: ${Byte.MIN_VALUE} to ${Byte.MAX_VALUE}"
 
+boolean flag = true
+println flag
+println flag.getClass()
+
+def n = 10
+println n.getClass().getName()
+
+// Type casting
+def n1 = (byte) 10
+println n1.getClass().getName()
+
+
 // List of data types:
 // byte
 // short
