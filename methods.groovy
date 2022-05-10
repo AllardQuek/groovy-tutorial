0557c67737dfbbeb293201dd9be41502960ae11d
Allard <AllardQuek@users.noreply.github.com>
2022-05-10 16:37:10 +0800

    Add methods

---
+++ methods.groovy
@@ -0,0 +1,15 @@
+// i.e. functions
+def sum(int a, int b) {
+	// Prints sum of a and b
+	println a + b
+}
+
+def sub(int a, int b) {
+	// Returns difference of a and b
+	def result = a - b
+	return result
+}
+	
+sum(2, 3)
+result = sub(3, 2)
+println result
