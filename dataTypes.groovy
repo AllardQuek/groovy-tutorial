b5ddc1525e948fcdd40ede5de356070763b6ee55
Allard <AllardQuek@users.noreply.github.com>
2022-05-10 15:23:38 +0800

    Add data types

---
+++ dataTypes.groovy
@@ -0,0 +1,14 @@
+byte b = 10
+println b
+println "range of byte: ${Byte.MIN_VALUE} to ${Byte.MAX_VALUE}"
+
+// List of data types:
+// byte
+// short
+// int
+// long
+// float
+// double
+// char
+// Boolean
+// String
